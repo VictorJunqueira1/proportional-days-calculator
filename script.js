eee99066cb81fceb419c001e467e9836cf9dee96
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-05-31 16:40:04 -0300

    fix: Refatorando lógica e cálculos, ajustando layout e incluindo responsividade.

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ document.getElementById('energyForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const energyDays = parseInt(document.getElementById('energyDays').value);
-    const energyStartDay = parseInt(document.getElementById('energyStartDay').value);
-    const energyEndDay = parseInt(document.getElementById('energyEndDay').value);
+    const energyStartDay = new Date(document.getElementById('energyStartDay').value);
+    const energyEndDay = new Date(document.getElementById('energyEndDay').value);
     const energyValue = parseFloat(document.getElementById('energyValue').value);
 
-    const tenantDays = energyEndDay - energyStartDay + 1;
-    const ownerDays = energyDays - tenantDays;
-
+    const tenantDays = Math.ceil((energyEndDay - energyStartDay) / (1000 * 60 * 60 * 24)) + 1;
     const dailyValue = energyValue / energyDays;
     const tenantValue = dailyValue * tenantDays;
-    const ownerValue = dailyValue * ownerDays;
+    const ownerValue = energyValue - tenantValue;
 
     document.getElementById('energyTenantValue').textContent = tenantValue.toFixed(2);
     document.getElementById('energyOwnerValue').textContent = ownerValue.toFixed(2);
@@ -20,17 +18,15 @@ document.getElementById('energyForm').addEventListener('submit', function (e) {
 document.getElementById('waterForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const waterStartDay = parseInt(document.getElementById('waterStartDay').value);
-    const waterEndDay = parseInt(document.getElementById('waterEndDay').value);
+    const waterDays = 30; // Dias fixos para a conta de água
+    const waterStartDay = new Date(document.getElementById('waterStartDay').value);
+    const waterEndDay = new Date(document.getElementById('waterEndDay').value);
     const waterValue = parseFloat(document.getElementById('waterValue').value);
 
-    const waterDays = 30; // Dias fixos para a conta de água
-    const tenantDays = waterEndDay - waterStartDay + 1;
-    const ownerDays = waterDays - tenantDays;
-
+    const tenantDays = Math.ceil((waterEndDay - waterStartDay) / (1000 * 60 * 60 * 24)) + 1;
     const dailyValue = waterValue / waterDays;
     const tenantValue = dailyValue * tenantDays;
-    const ownerValue = dailyValue * ownerDays;
+    const ownerValue = waterValue - tenantValue;
 
     document.getElementById('waterTenantValue').textContent = tenantValue.toFixed(2);
     document.getElementById('waterOwnerValue').textContent = ownerValue.toFixed(2);
